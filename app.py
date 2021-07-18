744056750a40e6eec6dbbebb488cbdef61337c26
oppecker <oppecker@gmail.com>
2021-07-18 13:41:53 -0400

    wording and spacing changes

--- app.py
+++ app.py
@@ -7,35 +7,35 @@ def ldap_connector():
 
 @app.route('/ldap_config')
 def ldap_config():
-    ''' Configure connection to ldap server '''
+    ''' Configure connection to ldap server. Save data for reuse. '''
     return 'Configure LDAP'
 
 @app.route('/tls_config')
 def tls_config():
-    ''' Configure connection to AWS API '''
+    ''' Configure connection to AWS API. Save data for reuse. '''
     return 'Configure TLS'
 
 @app.route('/run_audit')
 def run_audit():
-    ''' Get ldap and aws accounts, create alerts from differences, send alerts to API '''
+    ''' Get ldap and aws accounts, create alerts from differences, send alerts to API. '''
     ldap = get_ldap_accounts()
     aws = get_aws_accounts()
     alerts = _compare_accounts(ldap, aws)
     send_alerts(alerts)
-    return 'Run Audit'
+    return 'Running Audit'
 
 def get_ldap_accounts():
-    ''' Queries ldap server for list of current ldap users '''
+    ''' Queries ldap server for list of current ldap users. '''
     return []
 
 def get_aws_accounts():
-    ''' Queries AWS iam service for list of current aws users '''
+    ''' Queries AWS iam service for list of current aws users. '''
     return []
 
 def send_alerts(alerts):
-    ''' Uses TLS connection to send alert messages to cloud API'''
+    ''' Uses TLS connection to send alert messages to cloud API. '''
     pass
 
 def _compare_accounts(ldap, aws):
-    ''' Return alerts for accounts in AWS that are not in LDAP '''
+    ''' Return alerts for accounts in AWS that are not in LDAP. '''
     return []
