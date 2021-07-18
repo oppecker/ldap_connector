ecdc720b433fb6b81435108dcc4a4fe0253148b0
oppecker <oppecker@gmail.com>
2021-07-18 11:40:28 -0400

    removing powershell docker commands. use docker-compose instead

--- build_and_deploy.ps1
+++
@@ -1,2 +0,0 @@
-docker build --tag ldap_connector .
-docker run -d -p 5000:5000 --name connector_server ldap_connector
--- rebuild_and_deploy.ps1
+++
@@ -1,2 +0,0 @@
-& "$PSScriptRoot\cleanup.ps1"
-& "$PSScriptRoot\build_and_deploy.ps1"
