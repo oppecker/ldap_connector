76cc305660443853f468ba8487b3cb7e2bdd8c49
oppecker <oppecker@gmail.com>
2021-09-26 18:18:11 -0700

    adding configuration

--- ldap_connector/app.py
+++ ldap_connector/app.py
@@ -2,6 +2,7 @@ import ldap
 import os
 
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,18 +12,30 @@ def ldap_agent():
 @app.route('/ldap_config', methods = ['POST'])
 def ldap_config():
     ''' Configure connection to ldap server. Save data for reuse. '''
-    return 'Configure LDAP'
+    data = request.get_json()
+    if 'LDAP_ADMIN' in data:
+        os.environ['LDAP_ADMIN'] = data['LDAP_ADMIN']
+    if 'LDAP_ADMIN_PW' in data:
+        os.environ['LDAP_ADMIN_PW'] = data['LDAP_ADMIN_PW']
+    if 'LDAP_IP' in data:
+        os.environ['LDAP_IP'] = data['LDAP_IP']
+    return str(data)
 
-@app.route('/tls_config')
+@app.route('/tls_config', methods = ['POST'])
 def tls_config():
     ''' Configure connection to AWS API. Save data for reuse. '''
-    return 'Configure TLS'
+    data = request.get_json()
+    if 'API_IP' in data:
+        os.environ['API_IP'] = data['API_IP']
+    if 'API_TOKEN' in data:
+        os.environ['API_TOKEN'] = data['API_TOKEN']
+    return str(data)
 
 @app.route('/run_audit')
 def run_audit():
     ''' Get ldap and aws accounts, create alerts from differences, send alerts to API. '''
-    ldap = get_ldap_accounts()
-    aws = get_aws_accounts()
+    ldap = get_ldap_users()
+    aws = get_aws_users()
     alerts = _compare_accounts(ldap, aws)
     send_alerts(alerts)
     return 'Running Audit'
